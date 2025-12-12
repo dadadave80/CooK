60fc912631f4c04a0084f8a1e128f6c938ce9dee
David Dada <daveproxy80@gmail.com>
2025-12-12 08:57:02 +0100

    Update deploy script

--- script/00_DeployHook.s.sol
+++ script/00_DeployHook.s.sol
@@ -7,6 +7,34 @@ import {HookMiner} from "@uniswap/v4-periphery/src/utils/HookMiner.sol";
 import {BaseScript} from "./base/BaseScript.sol";
 
 import {Counter} from "../src/Counter.sol";
+import {IMarket, Market} from "../src/Market.sol";
+import {PotatoHook} from "../src/PotatoHook.sol";
+
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+
+contract DeployPotatoHookScript is BaseScript {
+    address constant MARKET = address(0x95e8136a95eDD41EEE8d2b2Eb9FA4E6216927378);
+    address constant USDC = address(0xE1B4C15D27Ae552FAF14A08C3b27DD46E780EA0F);
+
+    function run() public {
+        // hook contracts must have specific flags encoded in the address
+        uint160 flags = uint160(
+            Hooks.BEFORE_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG
+                | Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG | Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG
+        );
+
+        // Mine a salt that will produce a hook address with the correct flags
+        bytes memory constructorArgs = abi.encode(poolManager, MARKET, USDC);
+        (address hookAddress, bytes32 salt) =
+            HookMiner.find(CREATE2_FACTORY, flags, type(PotatoHook).creationCode, constructorArgs);
+
+        // Deploy the hook using CREATE2
+        vm.broadcast();
+        PotatoHook potatoHook = new PotatoHook{salt: salt}(poolManager, IMarket(MARKET), USDC);
+
+        require(address(potatoHook) == hookAddress, "DeployHookScript: Hook Address Mismatch");
+    }
+}
 
 /// @notice Mines the address and deploys the Counter.sol Hook contract
 contract DeployHookScript is BaseScript {
@@ -23,9 +51,8 @@ contract DeployHookScript is BaseScript {
             HookMiner.find(CREATE2_FACTORY, flags, type(Counter).creationCode, constructorArgs);
 
         // Deploy the hook using CREATE2
-        vm.startBroadcast();
+        vm.broadcast();
         Counter counter = new Counter{salt: salt}(poolManager);
-        vm.stopBroadcast();
 
         require(address(counter) == hookAddress, "DeployHookScript: Hook Address Mismatch");
     }
